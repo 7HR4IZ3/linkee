c60d1e6c9c035ca14157d63a3459fa2db3ae99ec
7HR4IZ3 <gamerxville@gmail.com>
2023-07-09 22:07:01 +0200

    Updated vercel support

--- core/urls.py
+++ core/urls.py
@@ -13,12 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("", include("main.urls")),
     path('admin/', admin.site.urls),
 ]
-urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
